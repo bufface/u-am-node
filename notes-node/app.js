c145c8d571a28c90b1fa09e9811d8a5564f0d047
bufface@gmail.com <bufface@gmail.com>
2017-03-14 22:18:05 -0300

    22. Listing Notes

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -7,11 +5,7 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 var argv = yargs.argv;
-
 var command = argv._[0];
-console.log('Command', command);
-console.log('Process', process.argv);
-console.log('Yargs', argv);
 
 if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body);
@@ -23,7 +17,9 @@ if (command === 'add') {
     console.log('No title taken');
   }
 } else if (command == 'list') {
-  notes.getAll();
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note));
 } else if (command == 'read') {
   var note = notes.getNote(argv.title);
 
