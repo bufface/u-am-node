2a770491ea0c5fe586fc3f2b28698fde15856656
bufface@gmail.com <bufface@gmail.com>
2017-03-18 16:01:45 -0300

    34. Wiring Up Weather Search

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,23 +1,39 @@
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode');
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if (errorMessage) {
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+// 4de04f0c7a975fd9e985bf091f7001fe
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if (errorMessage) {
-    console.log(errorMessage);
+const request = require('request');
+
+request({
+  url: 'https://api.forecast.io/forecast/4de04f0c7a975fd9e985bf091f7001fe/39.9396284,-75.18663959999999',
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('Unable to fetch weather.');
   }
 });
