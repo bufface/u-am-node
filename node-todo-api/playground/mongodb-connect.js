21387f04126452322eb618d056b5f973bfe40035
Cristian Buffa <bufface@gmail.com>
2017-06-25 19:13:35 -0300

    64. The ObjectId

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
@@ -13,22 +14,22 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   if (err) {
   //     return console.log('Unable to insert todo', err);
   //   }
-
+   
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
   // Insert new doc into Users (name, age, location)  
-  db.collection('Users').insertOne({
-    name: 'Cristian',
-    age: 30,
-    location: 'Buenos Aires'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert user', err);
-    }
+  // db.collection('Users').insertOne({
+  //   name: 'Cristian',
+  //   age: 30,
+  //   location: 'Buenos Aires'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
 
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
 
   db.close();  
 });
