e5cde7804b45ab66bceaedcff1846f1bcd8ba2a4
Cristian Buffa <bufface@gmail.com>
2017-07-23 15:48:20 -0300

    108. Emitting and Listening to Events (Solution)

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -3,9 +3,9 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createEmail', {
-    to: 'jen@example.com',
-    text: 'Hey. This is Cristian'
+  socket.emit('createMessage', {
+    from: 'Cristian',
+    text: 'Yup, that works for me.'
   });
 });
 
@@ -13,6 +13,6 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newEmail', function (email) {
-  console.log('New Email', email);
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
 });
